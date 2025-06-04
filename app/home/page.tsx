cdcb2c5bf875b30a3f0142a6a55241a21bb88eb1
Alejandro Pérez Durán <apduran95@gmail.com>
2025-06-04 16:47:18 -0400

    Update dependencies in package.json and pnpm-lock.yaml, add Toaster component from Sonner for notifications, and integrate AuthButton in HomePage for enhanced user authentication experience.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,9 @@
+import { AuthButton } from "@/components/auth/auth-button";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <AuthButton />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Geist } from "next/font/google";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -35,6 +36,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
